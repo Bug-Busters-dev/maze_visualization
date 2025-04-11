f79f30856505560050209b0d3fc3bb1f73d8e0f1
Apfelholz <apfelholzz1@gmail.com>
2025-04-11 17:16:07 +0200

    error

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 
-with open("data\labyrinthe0.txt", "r") as file:
+with open("data\\labyrinthe1.txt", "r") as file:
     lines = file.readlines()
-    (m, n) = lines[0].split(" ")
-    m = int(m)
-    n = int(n)
+    (x, y) = lines[0].split(" ")
+    y = int(y)
+    x = int(x)
 
 red =   "255 0   0   "
 white = "255 255 255 "
 schwarz = "0   0   0   "
 
-maze1 = [[[[white for i in range(3)]for i in range(3)] for i in range(n)]for i in range(m)]
+maze1 = [[[[white for i in range(3)]for i in range(3)] for i in range(y)]for i in range(x)]
 print(len(maze1), len(maze1[0]))
 
 def render_4d_list(data):
@@ -30,10 +30,25 @@ def render_4d_list(data):
 
 
 
-maze_description_len = 2*m-1
-number_mazes = int(lines[maze_description_len+1])
+maze_description_len = 2*y-1
+number_traps = int(lines[maze_description_len+1])
 
-traps = [[int(part.replace("\n", "")) for part in trap.split(" ")] for trap in lines[maze_description_len+2:maze_description_len+2+number_mazes]]
+traps = [[int(part.replace("\n", "")) for part in trap.split(" ")] for trap in lines[maze_description_len+2:maze_description_len+2+number_traps]]
+
+walls_v = [[int(lines[i].split(" ")[j].replace("\n", "")) for j in range(x-1)] + [1] for i in range(y)]
+
+walls_h = [[int(lines[i].split(" ")[j].replace("\n", "")) for j in range(x)] for i in range(y-1)] + [[1 for _ in range(y)]]
+
+for i in range(x):
+    for j in range(y):
+        if walls_v[i][j] == 1:
+            maze1[i][j][0][2] = schwarz
+            maze1[i][j][1][2] = schwarz
+            maze1[i][j][2][2] = schwarz
+        if walls_h[i][j] == 1:
+            maze1[i][j][2][0] = schwarz
+            maze1[i][j][2][1] = schwarz
+            maze1[i][j][2][2] = schwarz
 
 for x,y in traps:
     maze1[x][y][1][1] = red
@@ -41,17 +56,13 @@ for x,y in traps:
     maze1[x][y][2][0] = red
     maze1[x][y][0][2] = red
     maze1[x][y][2][2] = red
-    maze1[x][y][1][0] = schwarz
-    maze1[x][y][2][1] = schwarz
-    maze1[x][y][1][2] = schwarz
-    maze1[x][y][0][1] = schwarz
 
 
 
 with open("ouput_maze.ppm", "w") as file:
     string = ""
     file.write("P3\n")
-    file.write(f"{m*3} {n*3}\n")
+    file.write(f"{y*3} {x*3}\n")
     file.write("255\n")
 
 
